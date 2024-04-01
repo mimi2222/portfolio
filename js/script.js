4d75bf047c96b7a01cb351336c8a2a37113cf013
Mizuki Ogawa <sabrinacarpenter0310@gmail.com>
2024-04-01 14:58:17 -0700

    yatto iikanji

--- js/script.js
+++ js/script.js
@@ -9,3 +9,35 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach(element => {
+        element.classList.add('active');
+    });
+});
+
+function reveal() {
+    var reveals = document.querySelectorAll('.scroll');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+window.addEventListener('load', function() {
+    const iframe = document.getElementById('contact-me');
+    // オリジンが同じであれば、コンテンツの高さを取得して設定
+    iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+});
